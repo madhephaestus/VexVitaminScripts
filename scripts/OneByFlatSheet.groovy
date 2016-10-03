4db895c3b61aaeea64d6b3575a2108f841c280a1
Kevin Harrington <mad.hephaestus@gmail.com>
2016-10-03 09:07:01 -0400

    Update OneByFlatSheet.groovy

--- scripts/OneByFlatSheet.groovy
+++ scripts/OneByFlatSheet.groovy
@@ -1,1 +1,19 @@
 
+import eu.mihosoft.vrl.v3d.parametrics.*;
+CSG getNut(){
+	String type= "vexFlatSheet"
+	if(args==null)
+		args=["1x5"]
+	StringParameter size = new StringParameter(	type+" Default",
+										args.get(0),
+										Vitamins.listVitaminSizes(type))
+	//println "Database loaded "+database
+	HashMap<String,Object> servoConfig = Vitamins.getConfiguration( type,size.getStrValue())
+  
+  
+  
+	return <part>
+		.setParameter(size)
+		.setRegenerate({getNut()})
+}
+return getNut()
