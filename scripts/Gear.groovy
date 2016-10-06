d12f72e03585e6658326cb71e7f84728d18f8567
Andrew Turley <aturley123@gmail.com>
2016-10-06 13:43:54 -0400

    Added cylinder to the gear

--- scripts/Gear.groovy
+++ scripts/Gear.groovy
@@ -1,6 +1,34 @@
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 import eu.mihosoft.vrl.v3d.parametrics.*;
 
-CSG getGear() {
+CSG getGear(){
+	String type= "vexGear"
+	if (args==null) {
+		args=["84T"]
+	}
+	StringParameter size = new StringParameter(type + " Default",
+										args.get(0),
+										Vitamins.listVitaminSizes(type))
+	//println "Database loaded "+database
+	HashMap<String,Object> vexGearConfig = Vitamins.getConfiguration( type,size.getStrValue())
 
+	CSG round = new Cylinder(vexGearConfig.diameter/2, vexGearConfig.diameter/2, vexGearConfig.height, (int)30).toCSG()
+	
+  	/*CSG stub = sheet.difference(hole)
+  	CSG outputSheet = stub.clone()
+  	for (int i=0;i< vexSheetConfig.x;i++){
+  		for (int j=0;j<vexSheetConfig.y;j++){
+  			outputSheet = outputSheet.union(
+  					stub
+  						.movex(i*side)
+  						.movey(j*side)
+  					
+  				)
+  		}
+  	}*/
+  	//outputSheet = outputSheet.union(new Cube(side).toCSG())
+	return round
+		.setParameter(size)
+		.setRegenerate({getGear()})
 }
+return getGear()
