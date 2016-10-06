bdf4967576b7c0578066956281fbb83395391c9d
Philip R. Onffroy <datamaniac18@gmail.com>
2016-10-06 13:32:49 -0400

    Added a roundOrSquare If-Statement
    This checks if the wheel has a round or square edge and sets a boolean accordingly

--- scripts/Wheels.groovy
+++ scripts/Wheels.groovy
@@ -1,8 +1,8 @@
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins;
 import eu.mihosoft.vrl.v3d.parametrics.*;
 
-CSG getNut(){
-	String type= "vexWheels.json"
+CSG getWheel(){
+	String type= "vexWheels"
 	if(args==null)
 		args=["4inchStandard"]
 	StringParameter size = new StringParameter(	type+" Default",
@@ -10,16 +10,20 @@ CSG getNut(){
 										Vitamins.listVitaminSizes(type))
 	//println "Database loaded "+database
 	HashMap<String,Object> vexWheelConfig = Vitamins.getConfiguration( type,size.getStrValue())
-
-//boolean type = false 
-CSG mainBody = new Cylinder((vexWheelConfig.diameter)/2, // Radius at the bottom
-                       		(vexWheelConfig.diameter)/2, // Radius at the top
+	boolean roundEdge = false 
+	if(vexWheelConfig.squareOrRound.equals("round"))
+	{
+		 roundEdge = true 
+	}
+	//println roundEdge
+	CSG mainBody = new Cylinder((vexWheelConfig.diameter)/2, // Radius at the bottom
+    	                   		(vexWheelConfig.diameter)/2, // Radius at the top
                        		(vexWheelConfig.totalHeight), // Height
-                       		(int)30 //resolution
+                       		(int)40 //resolution
                        		).toCSG()//convert to CSG to display  
 
      return mainBody
 		.setParameter(size)
-		.setRegenerate({getNut()})
+		.setRegenerate({getWheel()})
 }
-return getNut()
+return getWheel()
