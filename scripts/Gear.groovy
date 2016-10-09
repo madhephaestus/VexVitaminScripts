674a53a898d2c6e673b771e42b6f9d8b3808e430
Andrew Turley <aturley123@gmail.com>
2016-10-09 12:11:30 -0400

    Added axle hole

--- scripts/Gear.groovy
+++ scripts/Gear.groovy
@@ -15,16 +15,22 @@ CSG getGear() {
 	double diameter = vexGearConfig.diameter
 	double height = vexGearConfig.height
 	double numTeeth = vexGearConfig.numTeeth
+	double toothLen = vexGearConfig.toothLen
 	
 	CSG round = new Cylinder(diameter/2, diameter/2, height, (int)84).toCSG()
 
-	CSG tooth = new Cube(10, 10, height).toCSG().movez(height/2)
-	tooth = tooth.difference(new Cube(7, 15, height).toCSG().rotz(-20).movex(-5).movez(height/2)).difference(new Cube(7, 15, height).toCSG().rotz(20).movex(5).movez(height/2)).movey(-diameter/2-4)
+	CSG tooth = new Cube(toothLen, toothLen, height).toCSG().movez(height/2)
+	tooth = tooth.difference(new Cube(7, 8, height).toCSG().rotz(-20).movex(-5).movez(height/2)).difference(new Cube(7, 8, height).toCSG().rotz(20).movex(5).movez(height/2)).movey(-diameter/2)
 	round = round.union(tooth)
 
+	CSG axleHole = new Cube(3.175, 3.175, height).toCSG().movez(height/2)
+	round = round.difference(axleHole)
+	
 	for (int i = 0; i < numTeeth; i++) {
 		round = round.union(tooth.clone().rotz(360/numTeeth*i))
 	}
+	round = round.movey(20)
+	round = round.setColor(javafx.scene.paint.Color.GREEN)
 	
 	return round
 		.setParameter(size)
