bcf248485c69e8935fece080ffbad9b7365e97cd
Andrew Turley <aturley123@gmail.com>
2016-10-09 12:20:23 -0400

    Added depth to gear

--- scripts/Gear.groovy
+++ scripts/Gear.groovy
@@ -16,15 +16,25 @@ CSG getGear() {
 	double height = vexGearConfig.height
 	double numTeeth = vexGearConfig.numTeeth
 	double toothLen = vexGearConfig.toothLen
+
+	CSG roundDepthBottom = new Cylinder(0, 0, 0).toCSG()
+	CSG roundDepthTop = new Cylinder(0, 0, 0).toCSG()
+	if (!(size.getStrValue() == "HS12T" || size.getStrValue() == "12T")) {
+		roundDepthBottom = new Cylinder(3*diameter/7, 3*diameter/7, height/4, (int)84).toCSG()
+		roundDepthTop = new Cylinder(3*diameter/7, 3*diameter/7, height/4, (int)84).toCSG().movez(height - height/4)
+	}
 	
 	CSG round = new Cylinder(diameter/2, diameter/2, height, (int)84).toCSG()
+
+	round = round.difference(roundDepthBottom).difference(roundDepthTop)
 
 	CSG tooth = new Cube(toothLen, toothLen, height).toCSG().movez(height/2)
 	tooth = tooth.difference(new Cube(7, 8, height).toCSG().rotz(-20).movex(-5).movez(height/2)).difference(new Cube(7, 8, height).toCSG().rotz(20).movex(5).movez(height/2)).movey(-diameter/2)
 	round = round.union(tooth)
 
 	CSG axleHole = new Cube(3.175, 3.175, height).toCSG().movez(height/2)
-	round = round.difference(axleHole)
+	CSG axleCylinder = new Cylinder(4, 4, height, (int)84).toCSG()
+	round = round.union(axleCylinder).difference(axleHole)
 	
 	for (int i = 0; i < numTeeth; i++) {
 		round = round.union(tooth.clone().rotz(360/numTeeth*i))
