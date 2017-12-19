6280e873f8f44e3233471597f2642ddf66dacf28
Ryan Benasutti <firey45@gmail.com>
2017-12-18 19:04:16 -0500

    Added LengthParameter for axle length

--- scripts/Motor.groovy
+++ scripts/Motor.groovy
@@ -27,15 +27,17 @@ CSG getMotor() {
 	post = post.hull();
 	post = post.movey(motor.getMaxY() - post.getMaxY()).movey(-postInset);
 	
-	CSG axle = new Cylinder(7.2/2, 7.2/2, 50.8, 80).toCSG();
-	axle = axle.movey(motor.getMinY() + axle.getTotalY()/2 + axleInset).movez(motor.getMaxZ());
-	
-	motor = motor.union(post);
-	motor = motor.union(axle);
-	
-	return motor
-		.setParameter(size)
-		.setRegenerate({getMotor()})
+        LengthParameter axleLength = new LengthParameter("Axle Length", 50.8, [10, 152.4]);
+        CSG axle = new Cylinder(7.2/2, 7.2/2, axleLength.getMM(), 80).toCSG();
+        axle = axle.movey(motor.getMinY() + axle.getTotalY()/2 + axleInset).movez(motor.getMaxZ());
+
+        motor = motor.union(post);
+        motor = motor.union(axle);
+
+        return motor
+                .setParameter(size)
+                .setParameter(axleLength)
+                .setRegenerate({getMotor()})
 }
 
 return getMotor()
